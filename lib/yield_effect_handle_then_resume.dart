932838d575492bf78a2a7ec2c893df11807ec443
Bent Hillerkus <bent@bent.party>
2026-08-08 02:30:35 +0200

    docs: some more code comments

--- lib/yield_effect_handle_then_resume.dart
+++ lib/yield_effect_handle_then_resume.dart
@@ -9,10 +9,27 @@ typedef BindState = Continuation<T> Function<T>(SetState<T> callback);
 
 /// Creates a coroutine that yields values of type [E] and can be resumed with values of type [T].
 typedef Computation<E extends BaseEffect> =
-    Iterable<E> Function(BindState receive);
+    Iterable<E> Function(BindState then);
 
 /// Wraps an [Iterable] of [BaseEffect]s so that it can only advance
 /// to the next yield, when the [BaseEffect] has been handled.
+/// 
+/// ```dart
+/// final doNothing = co((then) => sync* {
+///   yield Noop(then((_){});
+/// });
+/// ```
+/// 
+/// ```dart
+/// /// Probably looks just like this in the GitHub source code 🙃
+/// final loadIssueComments = co((then) => sync* {
+///   late final List<Comment> discussion;
+///   yield GetDiscussion(issueId, pagination, then((list) => discussion = list));
+///   if (Random.nextInt(10) < 9) { // 90% uptime 
+///     yield Respond(discussion, then((_){}));
+///   }
+/// });
+/// ```
 Iterable<E> co<E extends BaseEffect>(Computation<E> constructor) sync* {
   late void Function(void _) advance;
   // ignore: prefer_function_declarations_over_variables
@@ -34,7 +51,12 @@ Iterable<E> co<E extends BaseEffect>(Computation<E> constructor) sync* {
 
 /// Subclass your own effects as such
 /// ```dart
-/// class GetUser extends BaseEffect {
+/// /// Super class of all _your_ effects.
+/// ///
+/// /// Enables exhaustive pattern matching.
+/// sealed class Effect extends BaseEffect {}
+/// 
+/// class GetUser extends Effect {
 ///   GetUser(this.id, this.resume);
 ///   
 ///   final int id;
